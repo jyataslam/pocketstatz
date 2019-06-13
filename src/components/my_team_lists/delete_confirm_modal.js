e4cd9087b8cff076169725499320f519420ac2a5
Christopher Gustavsson <chrisgustavsson1@gmail.com>
2019-06-13 23:23:42 +0000

    finished styling modal applied media queries to modal

--- src/components/my_team_lists/delete_confirm_modal.js
+++ src/components/my_team_lists/delete_confirm_modal.js
@@ -13,10 +13,10 @@ class DeleteModal extends Component{
                         <div className="confirm-text center row">
                             <div className="col s10 offset-s1">Are you sure you want to remove {teamName} from your list?</div>
                         </div>
-                        <div className="confirm-btns center row">
+                        <div className="confirm-row center row">
                             <div className="col s10 offset-s1">
-                                <button onClick={() => {deleteTeam()}} className="btn btn-large green lighten-1">YES</button>
-                                <button onClick={() => {closeModal()}} className="btn btn-large red lighten-1">NO</button>
+                                <button onClick={() => {deleteTeam()}} className="btn confirm-btn green lighten-1">YES</button>
+                                <button onClick={() => {closeModal()}} className="btn deny-btn red lighten-1">NO</button>
                             </div>
                         </div>
                                 
