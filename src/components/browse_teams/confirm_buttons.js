5b83bd4e7c8d935494f22c47bd2df1f4c8950947
Christopher Gustavsson <chrisgustavsson1@gmail.com>
2019-06-14 22:35:16 +0000

    if guest has 3 teams selected, the select teams button on browse page will have a go to my teams button instead

--- src/components/browse_teams/confirm_buttons.js
+++ src/components/browse_teams/confirm_buttons.js
@@ -3,17 +3,26 @@ import React, { Component } from 'react';
 class DropDown extends Component {
 
     render() {
-        const { checkUserLoggedIn, selectedTeams } = this.props;
+        const { checkUserLoggedIn, selectedTeams, have3Teams, goToGuestTeams } = this.props;
         const margin = {"marginTop": "15px"};
 
-        if (selectedTeams.length !== 0){
-            return (
-                <button onClick={() => {checkUserLoggedIn()}} style={margin} className="btn btn-small green accent-4 col s10 offset-s1 pulse">Go To My Teams</button>
-            )
+        if (selectedTeams.length !== 0 || have3Teams){
+            if(have3Teams){
+                return(
+                    <button onClick={() => {goToGuestTeams()}} style={margin} className="btn btn-small green accent-4 col s10 offset-s1 pulse">Go To My Teams</button>
+
+                );
+            }
+            else{
+                return (
+                    <button onClick={() => {checkUserLoggedIn()}} style={margin} className="btn btn-small green accent-4 col s10 offset-s1 pulse">Go To My Teams</button>
+                );
+            }
+            
         } else {
             return (
                 <button onClick={() => {checkUserLoggedIn()}} style={margin} className="btn btn-small red lighten-3 accent-4 offset-s1 col s10">Select Your Teams</button>
-            )
+            );
         }
     }
 }
